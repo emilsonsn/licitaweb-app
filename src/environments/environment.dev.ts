a5185bd13329a2b6435c45d6c2a3eed32e3f46f8
emilsonsn <emilsonsn2@gmail.com>
2024-11-13 21:36:01 -0300

    fix: corrige envioments

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -4,7 +4,7 @@ export const environment = {
   production: false,
   appName: 'Licita Web',
   home: '/painel',
-  api: 'http://app-licitaweb.com.br:3001/api',
+  api: 'https://app-licitaweb.com.br:3001/api',
   // api: 'http://127.0.0.1:8000/api',
   version: require('../../package.json').version
 };
