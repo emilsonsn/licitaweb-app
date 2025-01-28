f4f222c6c9fdb2fec84943b106658d08a53adfc7
emilsonsn <emilsonsn2@gmail.com>
2025-01-28 09:36:08 -0300

    fix: corrige enviroments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@ export const environment = {
   production: false,
   appName: 'Licita Web',
   home: '/painel/home',
-  // api: 'https://app-licitaweb.com.br:3001/api',
-  api: 'http://127.0.0.1:8000/api',
+  api: 'https://app-licitaweb.com.br:3001/api',
+  // api: 'http://127.0.0.1:8000/api',
   apiLicita: "https://app.localizadordeeditais.com.br:3001/api/public/tender/search",
   version: require('../../package.json').version
 };
